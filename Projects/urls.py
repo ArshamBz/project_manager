ce2edb124c6bde91a4bc8864ffff5b2ab810b8f7
Arsham Bozorgi <arsham.bozorgi@gmail.com>
2024-10-07 19:20:39 +0330

    add project create and update and delete and add new models TASK and SUBTASK

--- Projects/urls.py
+++ Projects/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ProjectListView
+from .views import ProjectListView, ProjectCreateView, ProjectDeleteView, UpdateProjectView
 
 app_name = 'projects'
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='projects_list'),
+    path('create/project', ProjectCreateView.as_view(), name='project_create'),
+    path('delete/project/<int:pk>/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('update/project/<int:pk>/', UpdateProjectView.as_view(), name='project_update'),
 ]
--- Projects/views.py
+++ Projects/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Project
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import ProjectForm, ProjectUpdateForm
 
 
 class ProjectListView(LoginRequiredMixin, ListView):
@@ -16,3 +18,53 @@ class ProjectListView(LoginRequiredMixin, ListView):
             'projects': projects
         }
         return render(request, 'Projects/projects.html', context)
+
+
+class ProjectCreateView(CreateView):
+    """
+    this view give us create function for project model
+    """
+
+    form_class = ProjectForm
+    template_name = 'Projects/create_project.html'
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        form = self.form_class(request.POST, request.FILES,)
+        if form.is_valid():
+            data = form.cleaned_data
+            project = Project.objects.create(
+                user=user,
+                title=data['title'], image=data['image'],
+                description=data['description'], color=data['color'],
+                start_date=data['start_date'], end_date=data['end_date'],
+                budget=data['budget']
+            )
+            if project:
+                messages.success(request, 'your project saved', 'success')
+                return redirect('projects:projects_list')
+            messages.error(request, 'something went wrong !!!', 'danger')
+        else:
+            form = self.form_class()
+        return render(request, 'Projects/create_project.html', {"form": form})
+
+
+class ProjectDeleteView(DeleteView):
+    """
+        ...
+    """
+    model = Project
+    pk_url_kwarg = 'pk'
+    template_name = 'Projects/confirm_delete_pj.html'
+    success_url = '/'
+
+
+class UpdateProjectView(UpdateView):
+    """
+    ...
+    """
+    model = Project
+    form_class = ProjectUpdateForm
+    pk_url_kwarg = 'pk'
+    template_name = 'Projects/update.html'
+    success_url = '/'
